6ffb3074b4b4c7c53d366fcd568e6a75f458a543
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-01-10 11:06:39 +0900

    add:'capistrano-rvm'

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,3 +65,7 @@ namespace :deploy do
     end
   end
 end
+
+set :rvm_ruby_version, '1.9.3-p448'
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+SSHKit.config.command_map[:rake] = "#{fetch(:default_env)[:rvm_bin_path]}/rvm ruby-#{fetch(:rvm_ruby_version)} do bundle exec rake"
